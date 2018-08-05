151761180c27cb3278c423161b5485ad22d54607
Hanbin Hu <hanbinhu2016@gmail.com>
2018-08-04 22:12:57 -0700

    First commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential flex bison cmake cmake-curses-gui
+sudo apt-get install libboost-all-dev
+sudo apt-get install gnuplot graphviz doxygen exuberant-ctags
+sudo apt-get install tmux vim vim-gtk3
+sudo apt-get install dos2unix kdiff3 kompare htop
+sudo apt-get install git curl
+sudo apt-get install python3-dev python3-pip
+sudo apt-get install octave
+sudo apt-get install texlive-full texstudio
+sudo apt-get autoremove
+sudo apt-get autoclean
