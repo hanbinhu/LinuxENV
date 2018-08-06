c33a37f11dcaa62fad753f237b21d7ac692ae2fb
Hanbin Hu <hanbinhu2016@gmail.com>
2018-08-06 01:56:02 -0700

    Some Python3 Package added

--- .install.sh
+++ .install.sh
@@ -7,7 +7,8 @@ sudo apt-get install gnuplot graphviz doxygen exuberant-ctags
 sudo apt-get install tmux vim vim-gtk3
 sudo apt-get install dos2unix kdiff3 kompare htop
 sudo apt-get install git curl
-sudo apt-get install python3-dev python3-pip
+sudo apt-get install python3-dev python3-pip ipython3
+sudo apt-get install python3-numpy python3-scipy python3-sklearn python3-matplotlib
 sudo apt-get install octave
 sudo apt-get install texlive-full texstudio
 sudo apt-get autoremove
