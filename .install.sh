1612daf22a04bdcfb92d0ebf1d76b10da8a95685
Hanbin Hu <hanbinhu2016@gmail.com>
2018-08-07 03:31:39 -0700

    Library for tensorflow

--- .install.sh
+++ .install.sh
@@ -8,8 +8,9 @@ sudo apt-get install tmux vim vim-gtk3
 sudo apt-get install dos2unix kdiff3 kompare htop
 sudo apt-get install git curl
 sudo apt-get install python3-dev python3-pip ipython3
-sudo apt-get install python3-numpy python3-scipy python3-sklearn python3-matplotlib
+sudo apt-get install python3-numpy python3-scipy python3-sklearn python3-matplotlib python3-virtualenv
 sudo apt-get install octave
 sudo apt-get install texlive-full texstudio
+sudo apt-get install freeglut3 freeglut3-dev libxi-dev libxmu-dev
 sudo apt-get autoremove
 sudo apt-get autoclean
